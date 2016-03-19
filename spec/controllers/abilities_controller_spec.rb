d18f43203054f166b00989b98f363006bfeaffb2
Brian Campbell <briancmpbll@gmail.com>
2016-03-18 17:38:18 -0700

    Add tests for abilities controller response contents

--- spec/controllers/abilities_controller_spec.rb
+++ spec/controllers/abilities_controller_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AbilitiesController, type: :controller do
+  render_views
+
   it { is_expected.to route(:get, '/abilities').to(action: :index) }
   it { is_expected.to route(:get, '/abilities/1').to(action: :show, id: 1) }
 
   describe 'index' do
     before do
+      @abilities = FactoryGirl.create_list(:ability, 3)
       get :index, format: :json
     end
 
+    let(:results) { JSON.parse(response.body) }
+
     it { is_expected.to respond_with :success }
     it { is_expected.to render_template :index }
+    it { expect(results.length).to eq(@abilities.length) }
   end
 
   describe 'show' do
@@ -21,8 +27,7 @@ RSpec.describe AbilitiesController, type: :controller do
     context 'when the ability exists' do
       let(:ability) { FactoryGirl.create(:ability) }
       let(:ability_id) { ability.id }
-
-      subject(:results) { JSON.parse(response.body) }
+      let(:results) { JSON.parse(response.body) }
 
       it { is_expected.to respond_with :success }
       it { is_expected.to render_template :show }
