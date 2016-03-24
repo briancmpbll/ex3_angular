dccc51008a59d1ddcfe6b4d92abfacbe9c4b8053
Brian Campbell <briancmpbll@gmail.com>
2016-03-23 18:21:09 -0700

    Add validations for character attributes

--- app/models/character_attribute.rb
+++ app/models/character_attribute.rb
@@ -3,4 +3,12 @@ class CharacterAttribute < ActiveRecord::Base
   belongs_to :character
   belongs_to :attribute_data, class_name: 'Attribute',
                               foreign_key: 'attribute_id'
+
+  validates_presence_of :character
+  validates_presence_of :attribute_data
+
+  validates_uniqueness_of :attribute_data, scope: :character_id
+  validates_uniqueness_of :character, scope: :attribute_id
+
+  validates_inclusion_of :value, in: 0..5
 end
--- spec/models/character_attribute_spec.rb
+++ spec/models/character_attribute_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CharacterAttribute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @character_attribute = FactoryGirl.create(:character_attribute)
+  end
+
+  it_should_behave_like 'a timestamped model'
+
+  it 'should respond to fields' do
+    is_expected.to respond_to(:character, :attribute_data, :value)
+  end
+
+  it { is_expected.to validate_presence_of :character }
+  it { is_expected.to validate_presence_of :attribute_data }
+
+  it { is_expected.to validate_inclusion_of(:value).in_range(0..5) }
+
+  it 'should only allow one rating for each attribute on a character' do
+    is_expected.to validate_uniqueness_of(:attribute_data)
+      .scoped_to(:character_id)
+    is_expected.to validate_uniqueness_of(:character)
+      .scoped_to(:attribute_id)
+  end
 end
